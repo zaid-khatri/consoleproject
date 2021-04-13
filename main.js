16f26a4477294b59bf07555c8ac94bde7b4bf839
zaid-khatri <76276783+zaid-khatri@users.noreply.github.com>
2021-04-13 11:49:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log ("My first console project");
+var name="zaid";
+surname="khatri";
+fullname=name.concat(surname);
+console.log (fullname);
+console.log (15);
+console.log (5);
+console.log (50);
+console.log (2);
